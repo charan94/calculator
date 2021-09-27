84d4d9a2bf801a9af7ed52c115f712eb583e6685
Sai Charan Krishnagiri <sai.charan1994@gmail.com>
2021-09-27 17:51:50 +0530

    ADDED: Remining buttons

--- src/components/Block/index.js
+++ src/components/Block/index.js
@@ -4,27 +4,31 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 const Block = (props) => {
-    const { value, seperate, dark, value2, span } = props;
+    const { value, seperate, gray, value2, span, primary, bold } = props;
 
     const getSeperatedValues = () => {
         return (
-            <Row>
+            <Row className="position-relative">
                 <Col xs={6} className="text-center value-left">
                     <p className="mb-0">{value}</p>
                 </Col>
                 <Col xs={6} className="text-center">
-                    <p className="mb-0">{value2}</p>
+                    <p className="mb-0 text-secondary">{value2}</p>
                 </Col>
             </Row>
         );
     }
+
+    const color = gray ? 'gray' : primary ? 'primary' : '';
+
+    console.log('color ', color);
 
     const isSeperate = seperate && value2;
     const rowSpan = isSeperate ? span * 2 : span;
 
 
     return (
-        <Col xs={rowSpan} className={`my-2 p-2 custom-brd-radius ${dark ? 'bg-custom-gray' : ''} text-center`}>
+        <Col xs={rowSpan} className={`m-1 p-2 custom-brd-radius bg-custom-${color} text-center ${bold ? 'fw-bold' : ''}`}>
             {isSeperate ? getSeperatedValues() : value}
         </Col>
     );
--- src/components/CalcBlocks/index.js
+++ src/components/CalcBlocks/index.js
@@ -7,39 +7,65 @@ import Block from '../Block';
 const CalcBlocks = (props) => {
 
     return (
-        <Container fluid>
-            <Row>
+        <Container fluid className="mt-3">
+            <Row className="flex-nowrap">
                 <Col xs={5}>
-                    <Row>
-                        <Block value="Rad" value2="Deg" seperate dark span="4" />
-                        <Block value="x!" dark span="4" />
+                    <Row className="flex-nowrap">
+                        <Block value="Rad" value2="Deg" seperate gray span="4" />
+                        <Block value="x!" gray span="4" />
                     </Row>
-                    <Row>
-                        <Block value="Inv" dark span="4" />
-                        <Block value="sin" dark span="4" />
-                        <Block value="ln" dark span="4" />
+                    <Row className="flex-nowrap">
+                        <Block value="Inv" gray span="4" />
+                        <Block value="sin" gray span="4" />
+                        <Block value="ln" gray span="4" />
                     </Row>
-                    <Row>
-                        <Block value="π" dark span="4" />
-                        <Block value="cos" dark span="4" />
-                        <Block value="log" dark span="4" />
+                    <Row className="flex-nowrap">
+                        <Block value="π" gray span="4" />
+                        <Block value="cos" gray span="4" />
+                        <Block value="log" gray span="4" />
                     </Row>
-                    <Row>
-                        <Block value="e" dark span="4" />
-                        <Block value="tan" dark span="4" />
-                        <Block value="root" dark span="4" />
+                    <Row className="flex-nowrap">
+                        <Block value="e" gray span="4" />
+                        <Block value="tan" gray span="4" />
+                        <Block value="√ " gray span="4" />
                     </Row>
-                    <Row>
-                        <Block value="Ans" dark span="4" />
-                        <Block value="EXP" dark span="4" />
-                        <Block value="xy" dark span="4" />
+                    <Row className="flex-nowrap">
+                        <Block value="Ans" gray span="4" />
+                        <Block value="EXP" gray span="4" />
+                        <Block value="X" gray span="4" />
                     </Row>
                 </Col>
-                <Col xs={5}>
-
-                </Col>
-                <Col xs={2}>
-
+                <Col xs={6} className="mx-4">
+                    <Row className="flex-nowrap">
+                        <Block value="(" gray span="3" />
+                        <Block value=")" gray span="3" />
+                        <Block value="%" gray span="3" />
+                        <Block value="AC" gray span="3" />
+                    </Row>
+                    <Row className="flex-nowrap">
+                        <Block value="7" span="3" />
+                        <Block value="8" span="3" />
+                        <Block value="9" span="3" />
+                        <Block value="÷" gray span="3" />
+                    </Row>
+                    <Row className="flex-nowrap">
+                        <Block value="4" span="3" />
+                        <Block value="5" span="3" />
+                        <Block value="6" span="3" />
+                        <Block value="×" gray span="3" />
+                    </Row>
+                    <Row className="flex-nowrap">
+                        <Block value="1" span="3" />
+                        <Block value="2" span="3" />
+                        <Block value="3" span="3" />
+                        <Block value="-" gray span="3" />
+                    </Row>
+                    <Row className="flex-nowrap">
+                        <Block value="0" span="3" />
+                        <Block value="." span="3" bold />
+                        <Block value="=" primary span="3" />
+                        <Block value="+" gray span="3" />
+                    </Row>
                 </Col>
             </Row>
         </Container>
